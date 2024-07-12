2ae752b9576ae303f408209c7583b8e54ff27959
Syed Ali Hamza Naqvi <129213252+alihamza48@users.noreply.github.com>
2024-07-12 18:10:25 +0500

    Add files via upload

---
+++ UILocalization/UILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UILocalizationApp.swift
+//  UILocalization
+//
+//  Created by admin on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
